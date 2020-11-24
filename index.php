11130962671b6c6dff25bd1afb9bc5b9c339842f
Jonas Lopes <jonasmassume@gmail.com>
2020-11-23 21:32:29 -0300

    Teste da conexão com o banco de dados

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<body>
+<?php
+
+$hostname = "localhost";
+$username = "jonas";
+$password = "#Faex100193!";
+$db = "sistema_receita";
+
+$dbconnect=mysqli_connect($hostname,$username,$password,$db);
+
+if ($dbconnect->connect_error) {
+  die("Database connection failed: " . $dbconnect->connect_error);
+}
+
+?>
+
+<table border="1" align="center">
+<tr>
+  <td>Reviewer Name</td>
+  <td>Stars</td>
+  <td>Details</td>
+</tr>
+
+<?php
+
+$query = mysqli_query($dbconnect, "SELECT * FROM ingrediente")
+   or die (mysqli_error($dbconnect));
+
+while ($row = mysqli_fetch_array($query)) {
+  echo
+   "<tr>
+    <td>{$row['codigo']}</td>
+    <td>{$row['nome']}</td>
+    <td>{$row['quantidade']}</td>
+   </tr>\n";
+
+}
+
+?>
+</table>
+</body>
+</html>
